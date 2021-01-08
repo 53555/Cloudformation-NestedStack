6ebe9aaefe722783a24fbd0421712d0a9a63ea11
Silambarasan Karthikeyan <silambarasankarthikeyan@fico.com>
2021-01-08 16:31:16 +0530

    adding shell script to upload templates to S3 bucket

---
+++ s3_object_upload.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+CF_HOME=$1
+BUCKET_NAME=$2
+REGION_NAME=$3
+Network_Template=$(aws s3 ls s3://$BUCKET_NAME | awk -F" " '{print $4}'| grep NetworkStackStudent.yaml)
+IAM_Template=$(aws s3 ls s3://$BUCKET_NAME | awk -F" " '{print $4}'| grep IAMRole.yaml)
+
+if [[ $IAM_Template != "IAMRole.yaml" ]]
+then
+        aws s3 cp $1/IAMRole.yaml s3://$BUCKET_NAME/ --region $REGION_NAME
+else
+        echo "$IAM_Template already exist"
+fi
+
+if [[ $Network_Template != "NetworkStackStudent.yaml" ]]
+then
+        aws s3 cp $1/NetworkStackStudent.yaml s3://$BUCKET_NAME/ --region $REGION_NAME
+else
+        echo "$Network_Template already exist"
+fi
